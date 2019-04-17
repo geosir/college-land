fb7300742a6f68e2d0146f4a0c8b00ce79e28c39
George Moe <george@george.moe>
2019-04-17 12:07:42 -0400

    Finished working Cambridge scraper.

--- scraping/merge_owners.py
+++ scraping/merge_owners.py
@@ -3,9 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 import time
+from datetime import datetime
 
 
-def wait_cond(condition_fxn, pause=1, tries=60):
+def wait_cond(condition_fxn, pause=1, tries=600):
     for _ in range(tries):
         if not condition_fxn():
             time.sleep(pause)
@@ -15,8 +16,18 @@ def wait_cond(condition_fxn, pause=1, tries=60):
     raise Exception("Page load timeout.")
 
 
-def wait_element(classname, pause=0.5, tries=600):
+def wait_element(classname, pause=1, tries=600):
     return wait_cond(lambda: len(driver.find_elements_by_class_name(classname)) > 0, pause=pause, tries=tries)
+
+
+driver = None
+
+
+def init_driver():
+    global driver
+    options = Options()
+    # options.headless = True
+    driver = webdriver.Firefox(options=options)
 
 
 print("=============================================")
@@ -25,92 +36,142 @@ print("Copyright (c) 2019 George Moe")
 print("=============================================")
 
 print("Loading driver...")
-options = Options()
-# options.headless = True
-driver = webdriver.Firefox(options=options)
+init_driver()
 
 print("Loading Parcels...")
-with open("../data/CAMBRIDGE_PARCELS_FY2019.geojson") as f:
+# parcel_file = "../data/CAMBRIDGE_PARCELS_FY2019.geojson"
+parcel_file = "properties_merged_2-78_20190415.geojson"
+with open(parcel_file) as f:
     data = json.load(f)
 print("Found", len(data['features']), "features")
+
+start_property = 78
+print("Starting at property", start_property, "- Estimated scrape time:",
+      round((len(data['features']) - start_property + 1) * 6 / 3600, 2), "hours.")
 
 input("Press ENTER to start scrape.")
 
 print("========= SCRAPE START =========")
 try:
-    for feature in data['features'][1:]:
-        print("=== PROPERTY", feature['id'], ":", feature['properties']['MAP'], "-", feature['properties']['LOT'])
+    counter = 0
+    for feature in data['features'][start_property - 1:]:
+        try:
+            print("=== PROPERTY", feature['id'], ":", feature['properties']['MAP'], "-", feature['properties']['LOT'])
+            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
-        print("--> Get Property Search Form")
-        driver.get("https://www.cambridgema.gov/propertydatabase")
-        wait_cond(lambda: "Property Database" in driver.title)
+            feature['properties']['units'] = []
 
-        print("--> Enter Property ID")
-        elem = driver.find_element_by_name("bodycontent_0$txtAdvBlockNum")
-        elem.send_keys(feature['properties']['MAP'])
-        elem = driver.find_element_by_name("bodycontent_0$txtAdvLotNum")
-        elem.send_keys(feature['properties']['LOT'])
-        elem.send_keys(Keys.RETURN)
+            print("--> Get Property Search Form")
+            driver.get("https://www.cambridgema.gov/propertydatabase")
+            wait_cond(lambda: "Property Database" in driver.title)
 
-        wait_cond(lambda: "Search Again" in driver.page_source)
+            print("--> Enter Property ID")
+            elem = driver.find_element_by_name("bodycontent_0$txtAdvBlockNum")
+            elem.send_keys(feature['properties']['MAP'])
+            elem = driver.find_element_by_name("bodycontent_0$txtAdvLotNum")
+            elem.send_keys(feature['properties']['LOT'])
+            elem.send_keys(Keys.RETURN)
 
-        print("--> Result: ", end="")
-        inspect_queue = []
-        if driver.find_elements_by_id("bodycontent_0_gvSearchResults"):
-            print("Multiple properties.")
-            table = driver.find_element_by_id("bodycontent_0_gvSearchResults")
-            for link in table.find_elements_by_tag_name("a"):
-                href = link.get_attribute("href")
-                if "propertydatabase/" in href:
-                    print("    --#", href)
-                    inspect_queue.append(href)
-        else:
-            print("Single property.")
-            inspect_queue.append("THIS")
+            wait_cond(lambda: ("Search Again" in driver.page_source or
+                               driver.find_elements_by_id("bodycontent_0_lblNoResults")))
+            if driver.find_elements_by_id("bodycontent_0_lblNoResults"):
+                print("--> PROPERTY NOT FOUND.")
+                feature['properties']['merge_error'] = "NOT FOUND"
+                continue
 
-        feature['properties']['units'] = []
-        for page in inspect_queue:
-            unitdata = {}
-            print("--> Inspect", page)
+            print("--> Result: ", end="")
+            inspect_queue = []
+            if driver.find_elements_by_id("bodycontent_0_gvSearchResults"):
+                print("Multiple properties.")
+                table = driver.find_element_by_id("bodycontent_0_gvSearchResults")
+                for link in table.find_elements_by_tag_name("a"):
+                    href = link.get_attribute("href")
+                    if "propertydatabase/" in href:
+                        print("    --#", href)
+                        inspect_queue.append(href)
+            else:
+                print("Single property.")
+                inspect_queue.append("THIS")
 
-            if page != "THIS":
-                driver.get(page)
-            wait_cond(lambda: driver.find_elements_by_id("bodycontent_0_PropertyDetailPanel"))
-            info = driver.find_element_by_id("bodycontent_0_PropertyDetailPanel").find_element_by_class_name("leftCol")
+            for page in inspect_queue:
+                unitdata = {}
+                print("--> Inspect", page)
 
-            print("--> Getting data...")
-            for row in info.find_elements_by_tag_name("tr"):
-                if not (row.find_elements_by_tag_name("th") and row.find_elements_by_tag_name("td")):
-                    continue
+                url = page
+                if page != "THIS":
+                    driver.get(page)
+                else:
+                    url = driver.current_url
+                wait_cond(lambda: driver.find_elements_by_id("bodycontent_0_PropertyDetailPanel"))
+                inforows = driver.find_element_by_id("bodycontent_0_PropertyDetailPanel").find_element_by_class_name(
+                    "leftCol").find_elements_by_tag_name("tr")
 
-                key = row.find_element_by_tag_name("th")
-                value = row.find_element_by_tag_name("td")
-                # print(key.text, ":", value.text)
-                unitdata[key.text] = value.text
+                print("--> Getting data...")
+                fieldcount = 0
+                for row in inforows:
+                    if not (row.find_elements_by_tag_name("th") and row.find_elements_by_tag_name("td")):
+                        continue
 
-            unitdata['images'] = [image.get_attribute("src") for image in
-                                  driver.find_elements_by_class_name("propertyImage")]
+                    key = row.find_element_by_tag_name("th")
+                    value = row.find_element_by_tag_name("td")
+                    # print(key.text, ":", value.text)
+                    print(".", end="")
+                    unitdata[key.text] = value.text
+                    fieldcount += 1
+                print()
+                print("    --> Found", fieldcount, "fields.")
 
-            feature['properties']['units'].append(unitdata)
+                # Get images
+                unitdata['images'] = [image.get_attribute("src") for image in
+                                      driver.find_elements_by_class_name("propertyImage")]
+                print("    --> Found", len(unitdata['images']), "images.")
 
+                # Get sketches
+                unitdata['sketches'] = []
+                if driver.find_elements_by_id("bodycontent_0_pnlSketches"):
+                    unitdata['sketches'] = [image.get_attribute("src") for image in driver.find_element_by_id(
+                        "bodycontent_0_pnlSketches").find_elements_by_tag_name("img")]
+                print("    --> Found", len(unitdata['sketches']), "sketches.")
+
+                feature['properties']['units'].append(unitdata)
+
+                print("--> (Backoff)")
+                time.sleep(1)
+
+            print("---> Uniting key data...")
+            for field in ['Property Class', 'Assessed Value', 'Owner(s)']:
+                value = feature['properties']['units'][0][field]
+                print(field, ":", value)
+                feature['properties'][field] = value
+
+        except Exception as e:
+            print("EXCEPTION:", type(e).__name__, ":", e)
+            feature['properties']['merge_error'] = "{}: {}".format(type(e).__name__, e)
+            print("Skipping...")
+        finally:
             print("--> (Backoff)")
             time.sleep(1)
+            counter += 1
 
-        print("---> Uniting key data...")
-        for field in ['Property Class', 'Assessed Value', 'Owner(s)']:
-            value = feature['properties']['units'][0][field]
-            print(field, ":", value)
-            feature['properties'][field] = value
+            if counter % 100 == 0:
+                print("Writing data to file...")
+                with open('properties_merged.geojson', 'w') as outfile:
+                    json.dump(data, outfile)
 
-        print("--> (Backoff)")
-        time.sleep(1)
+                print("--> Reset driver to mitigate memory leak.")
+                driver.quit()
+                init_driver()
+
+
 
 except Exception as e:
-    print("EXCEPTION:", type(e).__name__, ":", e)
+    print("TOP-LEVEL EXCEPTION:", type(e).__name__, ":", e)
 except KeyboardInterrupt:
     print("Stop.")
+finally:
+    print("Writing data to file...")
+    with open('properties_merged.geojson', 'w') as outfile:
+        json.dump(data, outfile)
+    print("Done!")
 
-print("Writing data to file...")
-with open('properties_merged.geojson', 'w') as outfile:
-    json.dump(data, outfile)
-print("Done!")
+    driver.quit()
